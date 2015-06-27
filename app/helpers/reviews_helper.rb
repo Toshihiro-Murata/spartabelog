de5414adf31b71584fef9816975baa7a10259cf4
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-27 19:46:09 +0900

    Modify user checking

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
+
+  def already_reviewed? restaurant, current_user_id
+    restaurant.reviews.each do |review|
+      if review.user_id == current_user_id
+        return true
+      end
+    end
+    false
+  end
+
 end
