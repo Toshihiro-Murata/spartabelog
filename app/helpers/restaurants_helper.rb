d1a3945a975570dba2cd7520abe971ee2a1de859
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-27 19:55:48 +0900

    Add to calc the score

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,12 @@
 module RestaurantsHelper
+
+  def show_review_stars resutaurant
+    all_num = resutaurant.reviews.length
+    scores = 0
+    resutaurant.reviews.each do |review|
+      scores += review.score
+    end
+    scores / all_num
+  end
+
 end
