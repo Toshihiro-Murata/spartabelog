7b6eb576c6d132a727a92066736c1e721cbeb0cd
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-27 15:05:18 +0900

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(
+  name: "スパルタ亭",
+  tel: "03-0000-0000",
+  address: "東京都新宿区スパルタ亭１−１",
+  url: "http://spartatei.tei"
+)
+
+Restaurant.create(
+  name: "西新宿ラーメン",
+  tel: "03-0000-0001",
+  address: "東京都新宿区ラーメン町１−１",
+  url: "http://west-ramen.shinjuku"
+)
+
+Restaurant.create(
+  name: "都会焼肉",
+  tel: "03-0000-0002",
+  address: "東京都新宿区都会１−１",
+  url: "http://yakiniku.tokai"
+)
