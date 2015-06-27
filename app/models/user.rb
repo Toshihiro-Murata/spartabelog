dd7722f4c9ac568c55c9ac0f1cd56bc048be4529
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-27 16:04:00 +0900

    Add a valdation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name,
+    presence: true, length: { maximum: 20 }
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email,
+  #  presence: true, format: { with: VALID_EMAIL_REGEX }
 end
