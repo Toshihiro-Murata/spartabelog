cd87b6e2ba4f0226a5a7a973307a2d585fb9de6f
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-27 16:44:26 +0900

    Add a function for edit Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,37 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, { only: [:show, :edit]}
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :tel,
+      :address,
+      :url,
+    )
+  end
+
 end
