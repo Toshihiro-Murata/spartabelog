ecee1dd8fa28cdf429743a773ef025bd31cf3a1d
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-28 10:26:27 +0900

    Add showing star images

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,12 +1,27 @@
 module RestaurantsHelper
 
   def show_review_stars resutaurant
+    all_stars_num = 5
     all_num = resutaurant.reviews.length
     scores = 0
     resutaurant.reviews.each do |review|
       scores += review.score
     end
-    scores / all_num
+    colored_stars = (scores / all_num).round
+    case colored_stars
+    when 1
+      "/images/star_ll10.gif"
+    when 2
+      "/images/star_ll20.gif"
+    when 3
+      "/images/star_ll30.gif"
+    when 4
+      "/images/star_ll40.gif"
+    when 5
+      "/images/star_ll50.gif"
+    else
+      "/images/star_ll00.gif"
+    end
   end
 
 end
